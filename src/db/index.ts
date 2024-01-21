7f0e555e3d72047db7e7d40aa657cc1fdabb45ac
SahilRaj1 <raj24sahil@gmail.com>
2024-01-21 13:05:18 +0530

    db connection done

---
+++ src/db/index.ts
@@ -0,0 +1,15 @@
+import mongoose  from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () => {
+    try {
+        const options = {};
+        const connectionInstance = mongoose.connect(process.env.MONGODB_URI!, options);
+        console.log("MongoDB connected\nDB HOST : ", (await connectionInstance).connection.host);
+    } catch (error) {
+        console.error("MONGODB ERROR : ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
