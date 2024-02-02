525e39fb2730f756979638ece16a38b28a8353ae
SahilRaj1 <raj24sahil@gmail.com>
2024-02-02 07:56:41 +0530

    get channel controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -385,3 +385,80 @@ export const updateUserCoverImage = asyncHandler(
             );
     }
 );
+
+export const getUserChannelProfile = asyncHandler(
+    async (req: Request, res: Response) => {
+        const { username } = req.params;
+
+        if (!username?.trim()) {
+            return new ApiError(400, "Username is missing");
+        }
+
+        const channel = await User.aggregate([
+            {
+                $match: {
+                    username: username?.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedTo"
+                }
+            },
+            {
+                $addFields: {
+                    subscribersCount: {
+                        $size: "$subscribers"
+                    },
+                    channelsSubscribedToCount: {
+                        $size: "$subscribedTo"
+                    },
+                    isSubscribed: {
+                        $condition: {
+                            if: { $in: [(req as any).user?._id, "$subscribers.subscriber"] },
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    fullName: 1,
+                    username: 1,
+                    subscribersCount: 1,
+                    channelsSubscribedToCount: 1,
+                    isSubscribed: 1,
+                    avatar: 1,
+                    coverImage: 1,
+                    email: 1
+                }
+            }
+        ]);
+
+        if (!channel?.length) {
+            return new ApiError(400, "Channel does not exist");
+        }
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    channel[0],
+                    "User channel fetched successfully"
+                )
+            );
+    }
+);
