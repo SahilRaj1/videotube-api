dec3f8624eb3c9ee5e2df14da7a9468c10f0942e
SahilRaj1 <raj24sahil@gmail.com>
2024-02-04 15:31:18 +0530

    all models created

---
+++ src/models/like.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        likedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+        },
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: "Tweet",
+        },
+    },
+    { timestamps: true }
+);
+
+const Like = model("Like", likeSchema);
+
+export default Like;
